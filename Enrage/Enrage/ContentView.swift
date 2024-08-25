c7f8869b43474d2e912a8c8f9af27d0b360f2871
SAIREX <113220566+sairex53@users.noreply.github.com>
2024-08-25 21:04:38 +0700

    Upload Project

---
+++ Enrage/Enrage/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Enrage
+//
+//  Created by Elliot on 8/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Enrage()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Enrage/Enrage/EnrageApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnrageApp.swift
+//  Enrage
+//
+//  Created by Elliot on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct EnrageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
